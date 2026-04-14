d3515450ce236ceffdbe806bdb7cdde9167eb973
Amitha <gamitha.adiga@gmail.com>
2026-04-13 19:54:19 -0700

    Changes

--- src/test/java/com/saucedemo/runner/TestRunner.java
+++ src/test/java/com/saucedemo/runner/TestRunner.java
@@ -1,5 +1,19 @@
 package com.saucedemo.runner;
 
-public class TestRunner {
+import org.junit.platform.suite.api.*;
+import static io.cucumber.junit.platform.engine.Constants.*;
 
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(
+    key = PLUGIN_PROPERTY_NAME,
+    value = "pretty, html:target/cucumber-reports/report.html"
+)
+@ConfigurationParameter(
+    key = GLUE_PROPERTY_NAME,
+    value = "com.saucedemo.steps"
+)
+public class TestRunner {
+    // Empty — annotations do all the work!
 }
--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -6,3 +6,7 @@ Background:
 Scenario: Valid Login
 	When I login with username "standard_user" and password "secret_sauce"
 	Then I should be redirected to the inventory page
+	
+Scenario: Invalid Login
+	When I login with username "invlaid_username" and password "secret_sauce"
+	Then I should get a error message
